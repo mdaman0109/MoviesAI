a6902305a593301a83765abf78c705fe1623e82a
Md Aman <aman.md0109@gmail.com>
2025-07-03 01:34:12 +0530

    hosting issue

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// Example
+module.exports = {
+  content: [
+    './src/**/*.{html,js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
