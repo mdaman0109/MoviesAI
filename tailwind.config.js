4ffe28ea381e083cdfaa70ad7bb24708072f77fd
Md Aman <aman.md0109@gmail.com>
2025-07-03 02:21:09 +0530

    test

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
-// Example
-module.exports = {
- content: ['./src/**/*.{html,js,ts,jsx,tsx}'],
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './index.html',
+    './src/**/*.{js,jsx,ts,tsx}',
+  ],
   theme: {
     extend: {},
   },
