3d4e76ddda280aab880e0d0637f5ded5c6b15c86
Md Aman <aman.md0109@gmail.com>
2025-07-19 16:58:56 +0530

    responsiveness

--- src/Components/MovieList.jsx
+++ src/Components/MovieList.jsx
@@ -1,19 +1,28 @@
-import { useSelector } from "react-redux";
-import MovieList from "./MovieList";
+import MovieCard from "./MovieCard";
+import { Link } from "react-router";
 
-const SecondaryContainer = () => {
-  const movies = useSelector((store) => store.movies);
+const MovieList = ({ title, movies }) => {
+  if (!movies.length || !movies[0]?.poster_path) return null;
 
   return (
-    <div className="bg-black/60 backdrop-blur-sm rounded-4xl mt-[5vh] md:-mt-[5vh] lg:-mt-[2vh] xl:-mt-[10vh] m-0 sm:m-auto max-w-full py-3 md:py-3 relative z-20 -ml-4 sm:-ml-6 md:-ml-8">
-      <div className="space-y-3 max-w-full">
-        <MovieList title="Now Playing" movies={movies.nowPlayingMovies} />
-        <MovieList title="Popular Movies" movies={movies.popularMovies} />
-        <MovieList title="Top Rated Movies" movies={movies.topRatedMovies} />
-        <MovieList title="Upcoming" movies={movies.upcomingMovies} />
+    <div className="px-4 sm:px-6 md:px-8">
+      <h1 className="text-xl sm:text-2xl md:text-3xl text-white text-center md:text-left font-bold my-4">
+        {title}
+      </h1>
+
+      <div className="overflow-x-auto scroll-container w-full">
+        <div className="flex gap-4 px-2 sm:px-4 md:px-6 min-w-full">
+          {movies.map((movie) => (
+            <Link to={`/movieinfo/${movie?.id}`} key={movie?.id}>
+              <div className="flex-shrink-0 w-32 sm:w-40 md:w-48 lg:w-52">
+                <MovieCard poster_path={movie.poster_path} />
+              </div>
+            </Link>
+          ))}
+        </div>
       </div>
     </div>
   );
 };
 
-export default SecondaryContainer;
+export default MovieList;
