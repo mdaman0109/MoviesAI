c0cac991741f2f3c0383201f48ece8d3a86a2664
Md Aman <aman.md0109@gmail.com>
2025-07-20 03:48:31 +0530

    FINAL PUSH/SIGN OFF

--- src/Components/AISearchComponents/AISearch.jsx
+++ src/Components/AISearchComponents/AISearch.jsx
@@ -16,10 +16,10 @@ const AISearch = () => {
       />
 
       <div className="relative z-30 flex flex-col justify-start items-center pt-30 sm:pt-38 md:pt-32 lg:pt-40 xl:pt-44 px-2 sm:px-4 md:px-6 lg:px-8">
-        <h1 className="text-xl sm:text-2xl md:text-3xl lg:text-4xl p-4 rounded-4xl bg-black/20 backdrop-blur-sm border border-white/20 xl:text-5xl text-shadow-red-900 text-shadow-xs font-semibold text-center text-white  mb-4 sm:mb-5 md:mb-6 px-2 sm:px-4 leading-tight sm:leading-snug max-w-4xl">
-          {lang[langKey].welcomeTo} <span className="text-red-500 text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl text-shadow-white text-shadow-xs ring-red-500 ring-1 sm:ring-2 p-1 inline-block">&nbsp;{lang[langKey].AISearch}&nbsp;</span>
+        <h1 className="text-lg sm:text-xl md:text-2xl lg:text-3xl p-4 rounded-4xl bg-black/20 backdrop-blur-sm border border-white/20 xl:text-5xl text-shadow-red-900 text-shadow-xs font-semibold text-center text-white  mb-4 sm:mb-5 md:mb-6 px-2 sm:px-4 leading-tight sm:leading-snug w-[100vw] max-w-4xl">
+          {lang[langKey].welcomeTo} <span className="text-red-700 text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl text-shadow-gray-500 text-shadow-xs ring-red-700 ring-1  sm:ring-2 p-1 inline-block">&nbsp;{lang[langKey].AISearch}&nbsp;</span>
           <br className="hidden sm:block" />
-          <span className="block sm:inline text-white text-shadow-red-900 text-shadow-xs mt-2 sm:mt-0">
+          <span className="block sm:inline text-white text-shadow-red-900 text-lg sm:text-xl md:text-2xl lg:text-3xl text-shadow-xs mt-2 sm:mt-0">
             {lang[langKey].searchMessage}
           </span>
         </h1>
--- src/Utils/languageConstants.jsx
+++ src/Utils/languageConstants.jsx
@@ -3,21 +3,21 @@ const lang = {
     search: "Search",
     gptSearchPlaceholder: "What would you like to watch today?",
     welcomeTo:"Welcome To",
-    AISearch:"AISearch",
+    AISearch:"MoviesAI",
     searchMessage:"You can search for any movie you want to watch today"
   },
   hindi: {
     search: "खोज",
     gptSearchPlaceholder: "आज आप क्या देखना चाहेंगे?",
     welcomeTo:"आपका स्वागत है",
-    AISearch:"एआई खोज",
+    AISearch:"मूवीज़ एआई",
     searchMessage:"आप आज जो भी फिल्म देखना चाहते हैं, उसे खोज सकते हैं"
   },
   spanish: {
     search: "buscar",
     gptSearchPlaceholder: "¿Qué te gustaría ver hoy?",
     welcomeTo:"Bienvenido a",
-    AISearch:"Búsqueda AI",
+    AISearch:"Películas IA",
     searchMessage:"YPuedes buscar cualquier película que quieras ver hoy."
   },
 
@@ -26,7 +26,7 @@ const lang = {
   search: "Rechercher",
   gptSearchPlaceholder: "Qu’aimeriez-vous regarder aujourd’hui ?",
   welcomeTo: "Bienvenue à",
-  AISearch: "Recherche AI",
+  AISearch: "Films IA",
   searchMessage: "Vous pouvez rechercher n’importe quel film que vous souhaitez regarder aujourd’hui"
 },
 german:
@@ -34,7 +34,7 @@ german:
   search: "Suchen",
   gptSearchPlaceholder: "Was möchten Sie heute anschauen?",
   welcomeTo: "Willkommen bei",
-  AISearch: "KI-Suche",
+  AISearch: "Filme KI",
   searchMessage: "Sie können jeden Film suchen, den Sie heute ansehen möchten"
 },
 
@@ -43,14 +43,14 @@ german:
     search : "तलाशी",
     gptSearchPlaceholder : "आज रउरा का देखल चाहब?",
     welcomeTo:"राउर स्वागत बा",
-    AISearch:"एआईसर्च के बा",
+    AISearch:"फिलिम एआई",
     searchMessage:"आजु रउरा कवनो सिनेमा देखल चाहत बानी खोज कर सकीलें"
   },
 kannada: {
   search: "ಹುಡುಕು",
   gptSearchPlaceholder: "ಇಂದು ನೀವು ಏನು ವೀಕ್ಷಿಸಲು ಇಚ್ಛಿಸುತ್ತೀರಿ?",
   welcomeTo: "ಸ್ವಾಗತವಾಗಿದೆ",
-  AISearch: "AI ಹುಡುಕಾಟ",
+  AISearch: "ಚಲನಚಿತ್ರಗಳು AI",
   searchMessage: "ಇಂದು ನೀವು ವೀಕ್ಷಿಸಲು ಬಯಸುವ ಯಾವುದಾದರೂ ಸಿನಿಮಾವನ್ನು ಇಲ್ಲಿ ಹುಡುಕಬಹುದು"
 },
 bengali:
@@ -58,7 +58,7 @@ bengali:
     search : "অনুসন্ধান",
     gptSearchPlaceholder : "আজ তুমি কী দেখতে চাও?",
      welcomeTo:"স্বাগতম",
-    AISearch:"এআইসার্চ",
+    AISearch:"সিনেমা এআই",
     searchMessage:"আপনি আজ যে কোনও সিনেমা দেখতে চান তা অনুসন্ধান করতে পারেন।"
   },
 
@@ -68,7 +68,7 @@ tamil:
   search: "தேடு",
   gptSearchPlaceholder: "இன்று நீங்கள் பார்ப்பதற்காக விரும்புவது என்ன?",
   welcomeTo: "வரவேற்கிறோம்",
-  AISearch: "AI தேடல்",
+  AISearch: "திரைப்படங்கள் AI",
   searchMessage: "இன்று நீங்கள் பார்வையிட விரும்பும் எந்தத் திரைப்படத்தையும் இங்கு தேடலாம்"
 },
   
@@ -76,7 +76,7 @@ urdu: {
   search: "تلاش کریں",
   gptSearchPlaceholder: "آج آپ کیا دیکھنا چاہیں گے؟",
   welcomeTo: "خوش آمدید",
-  AISearch: "AI تلاش",
+  AISearch: "فلمیں AI",
   searchMessage: "آپ آج جو بھی فلم دیکھنا چاہتے ہیں، یہاں تلاش کر سکتے ہیں"
 },
 
@@ -85,7 +85,7 @@ marathi:
   search: "शोधा",
   gptSearchPlaceholder: "आज तुम्ही काय पाहायला आवडेल?",
   welcomeTo: "आपलं स्वागत आहे",
-  AISearch: "AI शोध",
+  AISearch: "चित्रपट AI",
   searchMessage: "आज तुम्हाला पाहायचा असलेला कोणताही चित्रपट इथे शोधू शकता"
 },
 
